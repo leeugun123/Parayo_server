22d09abf44159684f3c9441295eb000bcd73edd9
이유건 <50404123+leeugun123@users.noreply.github.com>
2022-08-16 22:00:25 +0900

    상품 등록 API

--- src/main/kotlin/com/example/parayo/domain/user/User.kt
+++ src/main/kotlin/com/example/parayo/domain/user/User.kt
@@ -1,5 +1,6 @@
 package com.example.parayo.domain.user
 
+import com.example.parayo.domain.jpa.BaseEntity
 import java.util.*
 import javax.persistence.*
 
@@ -8,21 +9,5 @@ import javax.persistence.*
 class User (
     var email: String,
     var password : String,
-    var name : String){
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    var id: Long? = null
-
-    var createdAt: Date? = null
-    var updatedAt: Date? = null
-
-    @PrePersist
-    fun prePersist(){
-        createdAt = Date()
-        updatedAt = Date()
-    }
-
-
-
+    var name : String) : BaseEntity(){
 }
