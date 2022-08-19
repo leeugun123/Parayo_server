e9267e19985928aacf7275a939e2197dfcc2c354
이유건 <50404123+leeugun123@users.noreply.github.com>
2022-08-19 18:15:59 +0900

    상품 리스트 API

--- src/main/kotlin/com/example/parayo/controller/ProductApiController.kt
+++ src/main/kotlin/com/example/parayo/controller/ProductApiController.kt
@@ -1,14 +1,14 @@
 package com.example.parayo.controller
 
 import com.example.parayo.common.ApiResponse
+import com.example.parayo.domain.product.Product
+import com.example.parayo.domain.product.ProductService
 import com.example.parayo.domain.product.registration.ProductImageService
 import com.example.parayo.domain.product.registration.ProductRegistrationRequest
 import com.example.parayo.domain.product.registration.ProductRegistrationService
+import com.example.parayo.domain.product.toProductListItemResponse
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 import org.springframework.web.multipart.MultipartFile
 
 
@@ -17,6 +17,7 @@ import org.springframework.web.multipart.MultipartFile
 class ProductApiController @Autowired constructor(
     private val productImageService: ProductImageService,
     private val productRegistration: ProductRegistrationService,
+    private val productService: ProductService
 ) {
 
     @PostMapping("/product_images")
@@ -30,4 +31,17 @@ class ProductApiController @Autowired constructor(
     ) = ApiResponse.ok(
         productRegistration.register(request)
     )
+
+
+    @GetMapping("/products")
+    fun search(
+        @RequestParam productId: Long,
+        @RequestParam(required = false) categoryId: Int?,
+        @RequestParam direction : String,
+        @RequestParam(required =  false) limit : Int?
+        ) = productService
+            .search(categoryId,productId,direction,limit ?: 10)
+        .mapNotNull(Product::toProductListItemResponse)
+        .let { ApiResponse.ok(it) }
+
 }
--- src/main/kotlin/com/example/parayo/domain/product/ProductRepository.kt
+++ src/main/kotlin/com/example/parayo/domain/product/ProductRepository.kt
@@ -1,6 +1,19 @@
 package com.example.parayo.domain.product
 
+import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface ProductRepository : JpaRepository<Product,Long> {
+
+    //1
+    fun findByCategoryIdAndIdGreaterThanOrderByIdDesc(
+        categoryId: Int, id : Long, pageable : Pageable
+    ): List<Product>
+
+    //2
+    fun findByCategoryIdAndIdLessThanOrderByIdDesc(
+        categoryId: Int,id : Long, pageable : Pageable
+    ): List<Product>
+
+
 }
